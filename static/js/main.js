6476e8f4364336b18014c1f8b65de7b61efd99b1
Emanuele Brivio <emanuele@plasticpanda.com>
2015-07-16 15:41:54 +0200

    Add stations and coord

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,61 @@
 /*jshint browser:true, indent:2, laxcomma:true, loopfunc: true, jquery: true */
-/*global $, console */
+/*global reqwest, console, Awesomplete, _ */
 
 (function () {
 
   'use strict';
-  
-  /*$('.retrieve-station').selectize({
-    valueField: 'url',
-    labelField: 'name',
-    searchField: 'name',
-    create: false,
-    render: {
-      option: function (item, escape) {
-        return item;
-      }
-    },
-    load: function (query, callback) {
-      if (!query.length) {
-        return callback();
-      }
-      $.ajax({
-        url: 'http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/' + encodeURIComponent(query),
-        type: 'GET',
-        crossDomain: true,
-        error: function () {
-          callback();
-        },
-        success: function (res) {
-          console.log(res);
-          callback(res);
-        }
-      });
-    }
-  });*/
+  var stations;
+      
+  var firstinput = document.querySelector('input[name="first"]');
+  var awesompletefirst = new Awesomplete(firstinput);
+  var lastinput = document.querySelector('input[name="last"]');
+  var awesompletelast = new Awesomplete(lastinput);
   
   reqwest({
-      url: 'http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/MILANO'
-    , method: 'get'
-    , crossOrigin: true
-    , error: function (err) { }
-    , success: function (resp) {
-        console.log(resp);
-      }
+    url: 'https://raw.githubusercontent.com/sabas/trenitalia/master/stazioni_coord.geojson',
+    type: 'json',
+    method: 'get',
+    crossOrigin: true,
+    error: function (err) { },
+    success: function (resp) {
+      
+      stations = _.map(resp.features, function (s) {
+        return {
+          nome: s.properties.name,
+          id_reg: s.properties.id_reg,
+          id_staz: s.properties.id_staz,
+          coords: s.geometry.coordinates,
+        };
+      });
+      
+      awesompletefirst.list = awesompletelast.list = _.pluck(stations, 'nome');
+      
+    }
+  });
+  
+  document.getElementById('save').addEventListener('click', function () {
+    var first = _.find(stations, { nome: firstinput.value });
+    var last = _.find(stations, { nome: lastinput.value });
+    
+    document.getElementById('message').textContent = '';
+    
+    if (first && last) {
+      // Everything is ok!
+      
+      var options = {
+        firstStationName: first.nome,
+        firstStationCoords: first.coords,
+        lastStationName: last.nome,
+        lastStationCoords: last.coords,
+      };
+      
+      document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
+      
+    } else {
+      // You mess with the wrong guy
+      document.getElementById('message').textContent = 'Verificare la correttezza dei campi inseriti';
+    }
+    
   });
 
 })();
